a03ba4466dc6eaea78d981d2975a430a53df8524
MP <markpaololibunao@gmail.com>
2021-05-18 06:40:58 +0800

    Returns validation errors

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -40,19 +40,55 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
-  @Mutation(() => User)
+  @Mutation(() => UserResponse)
   async register(
     @Arg('options') options: UsernamePasswordInput,
     @Ctx() { em }: MyContext
-  ) {
+  ): Promise<UserResponse> {
+    if (options.username.length <= 2) {
+      return {
+        errors: [
+          {
+            field: 'username',
+            message: 'length must be greater than 2',
+          },
+        ],
+      }
+    }
+
+    if (options.password.length <= 2) {
+      return {
+        errors: [
+          {
+            field: 'password',
+            message: 'length must be greater than 2',
+          },
+        ],
+      }
+    }
+
     const hashedPassword = await argon2.hash(options.password)
     const user = em.create(User, {
       username: options.username,
       password: hashedPassword,
     })
-    await em.persistAndFlush(user)
 
-    return user
+    try {
+      await em.persistAndFlush(user)
+    } catch (err) {
+      if (err.code === '23505' || err.detail.includes('already exists')) {
+        return {
+          errors: [
+            {
+              field: 'username',
+              message: 'username already taken',
+            },
+          ],
+        }
+      }
+    }
+
+    return { user }
   }
 
   @Mutation(() => UserResponse)
