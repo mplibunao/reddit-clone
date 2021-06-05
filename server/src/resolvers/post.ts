e2a9795e71ab2673b43c983cc642545357a04542
MP <markpaololibunao@gmail.com>
2021-06-06 06:52:05 +0800

    :art: Move sleep function to utils

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -2,8 +2,6 @@ import { Post } from '../entities'
 import { MyContext } from '../types'
 import { Arg, Ctx, Mutation, Query, Resolver } from 'type-graphql'
 
-//const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms))
-
 @Resolver()
 export class PostResolver {
   @Query(() => [Post])
