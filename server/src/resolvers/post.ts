cb6266d26e33717ebdae790c968a896c7d58a222
MP <markpaololibunao@gmail.com>
2021-09-06 15:01:28 +0800

    Implements cursor-based pagination on the backend for posts

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -11,6 +11,7 @@ import {
 } from 'type-graphql'
 import { MyContext } from '../types'
 import { isAuth } from '../middleware/isAuth'
+import { getConnection } from 'typeorm'
 
 @InputType()
 class PostInput {
@@ -23,8 +24,23 @@ class PostInput {
 @Resolver()
 export class PostResolver {
   @Query(() => [Post])
-  async posts(): Promise<Post[]> {
-    return Post.find()
+  async posts(
+    @Arg('limit') limit: number,
+    @Arg('cursor', () => String, { nullable: true }) cursor: string | null
+  ): Promise<Post[]> {
+    const realLimit = Math.min(50, limit)
+
+    const qb = getConnection()
+      .getRepository(Post)
+      .createQueryBuilder('p')
+      .orderBy('"createdAt"', 'DESC')
+      .take(realLimit)
+
+    if (cursor) {
+      qb.where('"createdAt" < :cursor', { cursor: new Date(parseInt(cursor)) })
+    }
+
+    return qb.getMany()
   }
 
   @Query(() => Post, { nullable: true })
