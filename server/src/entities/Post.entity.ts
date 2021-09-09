7713c9ebd182e45f439ddb9fd5e111cbe87a501f
MP <markpaololibunao@gmail.com>
2021-09-09 18:10:40 +0800

    Adds voteStatus (should be included in prev commit) field to determine currentUser's vote status

--- server/src/entities/Post.entity.ts
+++ server/src/entities/Post.entity.ts
@@ -8,7 +8,7 @@ import {
   ManyToOne,
   OneToMany,
 } from 'typeorm'
-import { Field, ObjectType } from 'type-graphql'
+import { Field, Int, ObjectType } from 'type-graphql'
 import { User } from './User.entity'
 import { Updoot } from './Updoot.entity'
 
@@ -30,6 +30,9 @@ export class Post extends BaseEntity {
   @Field()
   @Column({ type: 'int', default: 0 })
   points!: number
+
+  @Field(() => Int, { nullable: true })
+  voteStatus: number | null // 1 or -1 or null
 
   @Field()
   @Column('uuid')
