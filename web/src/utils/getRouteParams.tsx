1a29e43c7e8a4bd4258f48163a93e09712963ed8
MP <markpaololibunao@gmail.com>
2021-09-11 06:08:30 +0800

    Creates util function that extract integer route params

--- web/src/utils/getRouteParams.tsx
+++ web/src/utils/getRouteParams.tsx
@@ -5,3 +5,11 @@ export const getRouteParams = (field = 'id') => {
   const param = router.query[field]
   return typeof param === 'string' ? param : ''
 }
+
+export const useGetIntId = () => {
+  const router = useRouter()
+  const intId =
+    typeof router.query.id === 'string' ? parseInt(router.query.id) : -1
+
+  return intId
+}
